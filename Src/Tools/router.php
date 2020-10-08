6b590d0b8025d7cd958932947f1db6f35269e41c
SebH2as <se.haas@laposte.net>
2020-10-08 14:16:44 +0200

    router et extensions

---
+++ Src/Tools/router.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Projet5\Tools;
+
+use Projet5\Controller\MagController;
+use Projet5\Controller\ArticleController;
+use Projet5\Controller\UserController;
+use Projet5\Tools\Request;
+use Projet5\View\View;
+
+final class Router
+{
+    private Database $database;
+    private View $view;
+    private Request $request;
+
+    private $actionMag = 
+    [
+        'newMag',
+        'createNewMag',
+        'pannelMag',
+        'listMag',
+        'modifyMag',
+        'previewMag',
+        'addEdito',
+        'deleteMag',
+        'setOnlineMag',
+        'setSavedMag',
+        'previousMag',
+        'nextMag',
+        'magazine',
+        'readersLetters',
+        'previewLetters',
+        'previewEdito',
+        'editorial',
+        'whoWeAre'
+    ];
+
+    private $actionArticle =
+    [
+        'addContent',
+        'deleteArticle',
+        'createNewArticle',
+        'pannelArticle',
+        'modifyArticle',
+        'previewArticle',
+        'chroniques',
+        'essais',
+        'fictions',
+        'article',
+        'unsetMain',
+        'setMain'
+    ];
+
+    private $actionUser =
+    [
+        'monCompte',
+        'nousRejoindre',
+        'connectionPage',
+        'addUser',
+        'activation',
+        'connection',
+        'userDeco',
+        'nousEcrire',
+        'modifInfosUser',
+        'modifPassUser',
+        'modifEmailUser',
+        'modifPseudoUser',
+        'modifPass',
+        'modifEmail',
+        'modifPseudo',
+        'postLetter',
+        'newsLetterAbo',
+        'courrier',
+        'userLetter',
+        'relatedMag',
+        'addResponse',
+        'validation',
+        'courrierDelete',
+        'invalidation',
+        'adminProfil',
+        'reset',
+        'usersAdmin',
+        'deleteUser'
+    ];
+
+    public function __construct()
+    {
+        $this->database = new Database();
+        $this->view = new View();
+        $this->request = new request();
+    }
+
+    public function run(): void
+    {
+        if (($this->request->get('action')) !== null){
+            $key = array_search($this->request->get('action'), $this->actionMag);
+            $methode = $this->actionMag[$key]; 
+            if ($methode === $this->request->get('action')){
+                $controller = new magController();
+                $controller->$methode();
+                exit();
+            }
+            $key = array_search($this->request->get('action'), $this->actionArticle);
+            $methode = $this->actionArticle[$key]; 
+            if ($methode === $this->request->get('action')){
+                $controller = new articleController();
+                $controller->$methode();
+                exit();
+            }
+            $key = array_search($this->request->get('action'), $this->actionUser);
+            $methode = $this->actionUser[$key]; 
+            if ($methode === $this->request->get('action')){
+                $controller = new userController();
+                $controller->$methode();
+                exit();
+            }
+        }
+        $controller = new magController();
+        $controller->lastMagazine();
+    }
+
+}
+
+
+
+
+
+
+
+    
--- public/index.php
+++ public/index.php
@@ -1,112 +1,13 @@
 <?php
 require('../vendor/autoload.php');
 
+use Projet5\Tools\Router;
 
-use Projet5\Controller\MagController;
-use Projet5\Controller\ArticleController;
-use Projet5\Controller\UserController;
-use Projet5\Tools\Request;
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
 
 session_start();
 
-$request = new Request();
-
-$actionMag = 
-[
-    'newMag',
-    'createNewMag',
-    'pannelMag',
-    'listMag',
-    'modifyMag',
-    'previewMag',
-    'addEdito',
-    'deleteMag',
-    'setOnlineMag',
-    'setSavedMag',
-    'previousMag',
-    'nextMag',
-    'magazine',
-    'readersLetters',
-    'previewLetters',
-    'previewEdito',
-    'editorial',
-    'whoWeAre'
-];
-
-$actionArticle =
-[
-    'addContent',
-    'deleteArticle',
-    'createNewArticle',
-    'pannelArticle',
-    'modifyArticle',
-    'previewArticle',
-    'chroniques',
-    'essais',
-    'fictions',
-    'article',
-    'unsetMain',
-    'setMain'
-];
-
-$actionUser =
-[
-    'monCompte',
-    'nousRejoindre',
-    'connectionPage',
-    'addUser',
-    'activation',
-    'connection',
-    'userDeco',
-    'nousEcrire',
-    'modifInfosUser',
-    'modifPassUser',
-    'modifEmailUser',
-    'modifPseudoUser',
-    'modifPass',
-    'modifEmail',
-    'modifPseudo',
-    'postLetter',
-    'newsLetterAbo',
-    'courrier',
-    'userLetter',
-    'relatedMag',
-    'addResponse',
-    'validation',
-    'courrierDelete',
-    'invalidation',
-    'adminProfil',
-    'reset',
-    'usersAdmin',
-    'deleteUser'
-];
-
-if (($request->get('action')) !== null){
-    $key = array_search($request->get('action'), $actionMag);
-    $methode = $actionMag[$key]; 
-    if ($methode === $request->get('action')){
-        $controller = new magController();
-        $controller->$methode();
-        exit();
-    }
-    $key = array_search($request->get('action'), $actionArticle);
-    $methode = $actionArticle[$key]; 
-    if ($methode === $request->get('action')){
-        $controller = new articleController();
-        $controller->$methode();
-        exit();
-    }
-    $key = array_search($request->get('action'), $actionUser);
-    $methode = $actionUser[$key]; 
-    if ($methode === $request->get('action')){
-        $controller = new userController();
-        $controller->$methode();
-        exit();
-    }
-}
-
-
-$controller = new magController();
-$controller->lastMagazine();
-
-    
+$router = new Router();
+$router->run();
