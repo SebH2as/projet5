f7bd9d319305c717169693d804a15e1160dbc44b
SebH2as <se.haas@laposte.net>
2020-11-13 22:09:42 +0100

    loader

--- public/js/animation.js
+++ public/js/animation.js
@@ -4,18 +4,13 @@ let headerImg = document.getElementById('headerImg');
 let border = document.getElementById('border');
 let title01 = document.getElementById('title01');
 let title02 = document.getElementById('title02');
+let loader = document.getElementById('containLoader')
 
 
 
 window.onload = ()=>
 {
-    this.headerImg.style.opacity = "1";
-    this.headerImg.style.height = "100%";
-    this.border.style.height = "92%";
-    this.title01.style.opacity = "1";
-    this.title02.style.opacity = "1";
-    
-    
+    document.getElementById('containLoader').parentElement.removeChild(document.getElementById('containLoader'));
     
 }
 
