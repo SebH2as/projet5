99b38fcd6c00f8bd68d72d112a7d04719a8b7174
SebH2as <se.haas@laposte.net>
2020-11-16 16:57:14 +0100

    css corrections

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,16 +1,5 @@
-let left = document.getElementById('arrowLeft');
-let right = document.getElementById('arrowRight');
-let headerImg = document.getElementById('headerImg');
-let border = document.getElementById('border');
-let title01 = document.getElementById('title01');
-let title02 = document.getElementById('title02');
-let loader = document.getElementById('containLoader')
-
-
-
 window.onload = ()=>
 {
     document.getElementById('containLoader').parentElement.removeChild(document.getElementById('containLoader'));
-    
 }
 
